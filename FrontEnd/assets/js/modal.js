af83fe1b634c0c36f49d997a586a3cd2370611ec
Axel Le Meur <95547308+fahrenheigt@users.noreply.github.com>
2023-11-29 14:50:22 +0100

    Création du document Modal et des constantes pour ajout d'image

---
+++ FrontEnd/assets/js/modal.js
@@ -0,0 +1,18 @@
+// constantes pour la modal suppression de projets
+const modalDeleteWork = document.querySelector("#modalsSuppr");
+const openGalleryModalBtn = document.querySelector("#projectEdit");
+const closeGalleryModalBtn = document.querySelector("#fermer-suppr");
+
+//constantes pour la modal ajout de projets
+const modalAddWork = document.querySelector ("#modalsAjout");
+const openAddWork = document.querySelector ("#AjoutPhoto");
+const previousBtn = document.querySelector ("#.precedent");
+const closeAddWorkModalBtn = document.querySelector ("#fermer-ajout");
+
+//constantes pour upload image 
+const uploadImageInput = document.querySelector ("#imageUpload");
+const projetctUpload = document.querySelector ("#previewImage");
+const uploadContent = document.querySelector("#previewdetails");
+const submitProjet = document.querySelector("#validerAjout");
+const backgroundPreview = document.querySelector(".AjoutPhotoContainer");
+const addProjectForm = document.querySelector("#ajout-form");
