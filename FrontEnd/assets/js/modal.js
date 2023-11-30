ed2ded5579ada0e1ee0ee3d92f707f10ca550e7d
Axel Le Meur <95547308+fahrenheigt@users.noreply.github.com>
2023-11-30 18:37:31 +0100

    Update modal.js

--- FrontEnd/assets/js/modal.js
+++ FrontEnd/assets/js/modal.js
@@ -1,20 +1,21 @@
-// constantes pour la modal suppression de projets
+// Variables pour la modal suppression de projets
 const modalDeleteWork = document.querySelector("#modalsSuppr");
 const openGalleryModalBtn = document.querySelector("#projectEdit");
 const closeGalleryModalBtn = document.querySelector("#fermer-suppr");
 
-//constantes pour la modal ajout de projets
-const modalAddWork = document.querySelector ("#modalsAjout");
-const openAddWork = document.querySelector ("#AjoutPhoto");
-const previousBtn = document.querySelector ("#.precedent");
-const closeAddWorkModalBtn = document.querySelector ("#fermer-ajout");
+// Variables pour la modal ajout de projets
+const modalAddWork = document.querySelector("#modalsAjout");
+const openAddWork = document.querySelector("#AjoutPhoto");
+const previousBtn = document.querySelector(".precedent");
+const closeAddWorkModalBtn = document.querySelector("#fermer-ajout")
 
-//constantes pour upload image 
-const uploadImageInput = document.querySelector ("#imageUpload");
-const projetctUpload = document.querySelector ("#previewImage");
+// Variables pour upload une image
+const uploadImageInput = document.querySelector("#imageUpload");
+const projectUpload = document.querySelector("#previewImage");
 const uploadContent = document.querySelector("#previewdetails");
 const submitProjet = document.querySelector("#validerAjout");
 const backgroundPreview = document.querySelector(".AjoutPhotoContainer");
+
 const addProjectForm = document.querySelector("#ajout-form");
 
 // Variable pour background modal
